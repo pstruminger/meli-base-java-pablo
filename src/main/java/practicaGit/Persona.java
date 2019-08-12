84e2a3fbb20ae3ac9c5f08031241d40a9d0bcc52
Pablo <pablo.struminger@mercadolibre.com>
2019-08-12 15:07:18 -0300

    Se agrega clase persona

---
+++ src/main/java/practicaGit/Persona.java
@@ -0,0 +1,13 @@
+package practicaGit;
+
+public class Persona {
+
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+}
