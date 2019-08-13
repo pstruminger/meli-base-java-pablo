414d06456d9660e39c11324cf61d7095558b5e89
Pablo <pablo.struminger@mercadolibre.com>
2019-08-13 12:02:01 -0300

    cambios en pom, arreglado problema de clases, cambios en travis.yml

--- src/main/java/interfaces/Hablador.java
+++
@@ -1,5 +0,0 @@
-package interfaces;
-
-public interface Hablador {
-    public String hablar(int veces);
-}
--- src/main/java/interfaces/Perro.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public class Perro implements Hablador {
-
-    @Override
-    public String hablar() {
-        return "guau";
-    }
-}
